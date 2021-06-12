fa0f4d4df6ee20bed7f742f8dedf4d36047b4191
Muhammad-Topan-Ramadona <Topanthecool24@gmail.com>
2021-06-12 21:32:45 +0700

    Operasi Tipe Data String

--- Modul1_IDEA/src/Main.java
+++ Modul1_IDEA/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
         public static void main(String[]args){
                 String namaDepan = "Muhammad";
@@ -16,5 +18,22 @@ public class Main {
                 String uniskaString = new String(uniskaChar);
                 System.out.println(uniskaChar);
                 System.out.println(uniskaString);
+
+                String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
+                System.out.println(namaLengkap);
+                System.out.println(namaLengkap.length());
+                System.out.println(namaLengkap.indexOf("don"));
+                System.out.println(namaLengkap.substring(15));
+                System.out.println(namaLengkap.substring(15,20));
+                System.out.println(namaLengkap.replace("Top","Erf"));
+                System.out.println(namaLengkap.toLowerCase());
+                System.out.println(namaLengkap.toUpperCase());
+                System.out.println(namaLengkap.charAt(15));
+
+                String[] namaArray = namaLengkap.split(" ");
+
+                for(String nama : namaArray)
+                        System.out.println(nama);
         }
 }
