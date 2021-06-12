e253d54d93b1490f4e338ba0872e9423af76c126
Muhammad-Topan-Ramadona <Topanthecool24@gmail.com>
2021-06-12 19:11:34 +0700

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -1,14 +1,24 @@
 public class DataTypes{
  public static void main(String[]args){
-
-  String namaDepan = "Mirza";
-  String namaTengah = "Yogy";
-  String namaBelakang = "Kurniawan";
-  int usia = 18;
+  String namaDepan = "Muhammad";
+  String namaTengah = "Topan";
+  String namaBelakang = "Ramadona";
+  int usia = 19;
   int targetTahunKuliah = 4;
   double ipk = 3.89764512;
   char nilaiAbjad = 'A';
   boolean tampan = true;
   
+  System.out.println("========== OUTPUT ==========");
+  System.out.println("Nama Depan : " + namaDepan );
+  System.out.println("Nama Tengah : " + namaTengah );
+  System.out.println("Nama Belakang : " + namaBelakang );
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai PBO : " + nilaiAbjad);
+  System.out.println("Tampan : " + tampan);
+  
  }
+ 
 }
